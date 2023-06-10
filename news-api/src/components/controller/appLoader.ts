f4d4a70246d87cfc201dda4b5b6687cf78ecc71c
Aleksey Pashchuk <aleksey.pashchuk@gmail.com>
2023-06-10 20:25:11 +0300

    test build

--- news-api/src/components/controller/appLoader.ts
+++ news-api/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://nodenews.herokuapp.com/', {
+        super('https://rss-news-api.onrender.com/', {
             apiKey: 'b3095f32b45e4809b0761a77753ace53',
         });
     }
